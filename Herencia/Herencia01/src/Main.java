d45226e64c14ba9276477efb79e9cddde2cfd63b
LuisJaramillo <luisjaramillopulido@gmail.com>
2022-04-28 11:05:47 +0200

    agregado ejemplo03

--- Herencia/Herencia01/src/Main.java
+++ Herencia/Herencia01/src/Main.java
@@ -3,7 +3,7 @@ public class Main {
 
 	public static void main(String[] args) {
 		Alumno al = new Alumno();
-		al.setNombre("opra");
+		al.setNombre("Bill Gates");
 		al.setDni(12312312);
 		al.setCreditosAprobados(9);
 		System.out.println(al);
